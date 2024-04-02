e147cdcb97ed4f647be526fa0ede449a069e5a4f
Werner <wstablein@hotmail.com>
2024-04-02 15:13:27 +0200

    Add user request request loop to repeat request for data until data provided is valid

--- run.py
+++ run.py
@@ -29,14 +29,26 @@ def get_sales_data():
     """
     Get sales figures input from the user
     """
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers")
-    print("Example: 10,20,30,40,50,60\n")
 
-    data_str = input("Enter your data here: ")
-    print(f"The data provided is {data_str}")
+    def get_sales_data():
+        """
+        Get sales figures input from the user.
+        """
 
-    
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
+
+        data_str = input("Enter your data here: ")
+
+        sales_data = data_str.split(",")
+
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break
+
+    return sales_data
 
 # pick the data entered as string and split them into a list
 # reason why: data must be in a list to update the google sheet
@@ -46,18 +58,21 @@ def get_sales_data():
 
 def validate_data(values):
     """
-    Inside the try converts all string values into integers
-    Returns valueError if strings cannot be converted into int
-    or if there aren't exactly 6 values
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted into int,
+    or if there aren't exactly 6 values.
     """
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
-                f"We expect 6 values, you provided {len(values)}"
+                f"Exactly 6 values required, you provided {len(values)}"
             )
     except ValueError as e:
-
         print(f"Invalid data: {e}, please try again.\n")
-    
+        return False
 
-get_sales_data()
+    return True
+
+
+data = get_sales_data()
